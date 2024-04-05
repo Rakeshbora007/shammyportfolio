3eaf29391cc391ec4527bf51f5de47f0849e8ff2
Rakeshbora007 <www.rakeshbora365@gmail.com>
2024-04-05 17:45:16 +0530

    commit

--- src/components/home/Social.jsx
+++ src/components/home/Social.jsx
@@ -2,15 +2,15 @@ import React from "react";
 const Social = () =>{
     return(
         <div className="home__social">
-            <a href="https://www.instagram.com/malvi_kartik09/" className="home__social-icon" target="_blank" rel="noopener">
+            <a href="https://www.instagram.com/" className="home__social-icon" target="_blank" rel="noopener">
             <i class="uil uil-instagram"></i>
             </a>
 
-            <a href="https://www.linkedin.com/in/kartik-malviya-3a9a821b2/" className="home__social-icon" target="_blank" rel="noopener">
+            <a href="https://www.linkedin.com/" className="home__social-icon" target="_blank" rel="noopener">
             <i class="uil uil-linkedin"></i>
             </a>
 
-            <a href="https://github.com/kartik-malvi" className="home__social-icon" target="_blank" rel="noopener">
+            <a href="https://github.com/" className="home__social-icon" target="_blank" rel="noopener">
             <i class="uil uil-github-alt"></i>
             </a>
         </div>
